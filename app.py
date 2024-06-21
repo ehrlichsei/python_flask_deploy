8ca1d11471627ff915751e98623000ec6e01dda3
yuli.fang <fangnuaa@gmail.com>
2024-06-21 18:43:29 +0200

    update port

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 # app.py
+import os
 from flask import Flask
 
 app = Flask(__name__)
@@ -7,5 +8,8 @@ app = Flask(__name__)
 def hello():
     return 'Hello, World!'
 
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Heroku will set PORT environment variable
+    port = int(os.environ.get('PORT', 5001))
+    app.run(host='0.0.0.0', port=port, debug=True)
